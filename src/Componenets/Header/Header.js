64bc3b8ceae252796bf6fecac98583e171867ac0
mynuddineee <mynuddineee@gmail.com>
2021-09-26 08:41:55 +0600

    first commit

---
+++ src/Componenets/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Bangladesh Pharmaceuticals Ltd</h2>
+            <p>One of the best Pharmaceuticals company in Bangladesh</p>
+            <h4>Salary Budget Yearly BDT 5000000</h4>
+        </div>
+    );
+};
+
+export default Header;
